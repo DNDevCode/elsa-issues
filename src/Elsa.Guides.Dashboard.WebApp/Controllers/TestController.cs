4a2eae1f58b79abd74957ed8a734e286d2391216
Nadir <Nadir@ALP-PC-DEV14>
2021-03-01 08:40:03 +0100

    Ajoutez des fichiers projet.

---
+++ src/Elsa.Guides.Dashboard.WebApp/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using Elsa.Activities.Workflows.Extensions;
+using Elsa.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elsa.Guides.Dashboard.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IWorkflowInvoker _invoker;
+
+        public TestController(IWorkflowInvoker invoker)
+        {
+            _invoker = invoker;
+        }
+
+        [HttpPost("initialize")]
+        public async Task<IActionResult> Intitiaize()
+        {
+            await _invoker.TriggerSignalAsync("INITIALIZE");
+            return Ok();
+        }
+
+        [HttpPost("signal-b")]
+        public async Task<IActionResult> SignalB()
+        {
+            await _invoker.TriggerSignalAsync("SIGNAL_B");
+            return Ok();
+        }
+
+    }
+}
